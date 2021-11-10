f9823bbecc15771d9c62a740b50bfa12c11411b5
RodinaMariia <79466116+RodinaMariia@users.noreply.github.com>
2021-11-10 11:35:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 """
-API for multivariate forecast.
-
-The main function: make predictions to a several steps forward from the last date in the fitted data.
+Entrypoint for a fast calculations. Current example initialize manager with a some data and make predictions to a three steps forward.
 """
 import os
 import logging
@@ -12,21 +10,9 @@ from datetime import datetime
 from timelibs.app import app
 from timelibs.timepredictor import SalesCompound, load_pickle, save_pickle
 
-if __name__ == '__main__':
-    #
-    #cmp = SalesCompound()
-    #cmp.initial(pd.read_csv(os.path.join('data/', "sales.csv"),
-    #                    sep=";", parse_dates=['Date'], dayfirst=True))
-    #save_pickle(cmp, 'compound_short')
-
-    cmp = load_pickle('compound_short')
-    # for key, value in cmp.size_predictors.items():
-    #     t = cmp.encoders.get('Nomenclature_size').inverse_transform([key])
-    #     print('{}: {}'.format(cmp.encoders.get('Nomenclature_size').inverse_transform([key])[0],
-    #                           value))
-    #preds = cmp.size_predictors.get(24).predict(3, 24, cmp.all_sizes.query('Nomenclature_size==24').Nomenclature_group.values[0])
-
-    # cmp.fit(pd.read_csv(os.path.join('data/', "sales.csv"),
-    #                     sep=";", parse_dates=['Date'], dayfirst=True))
-    preds = cmp.predict(3)
-    print(preds)
+if __name__ == '__main__':    
+    cmp = SalesCompound()
+    cmp.initial(pd.read_csv(os.path.join('data/', "sales.csv"),
+                        sep=";", parse_dates=['Date'], dayfirst=True))
+    save_pickle(cmp, 'compound_short')    
+    print(cmp.predict(3))
